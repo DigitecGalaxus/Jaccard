24621b60b8b149125ccf16eb00f95312cd187c9a
Zenit <zenit.kovacevic@gmail.com>
2019-09-02 21:48:21 +0200

    Initial Commit

---
+++ JaccardCalculator.Test/CalculateJaccardIndexTest.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace JaccardCalculator.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void CalculateJaccardIndex_ExpectArgumentException()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => JaccardIndex.CalculateJaccardIndex(new[] {0}, null));
+
+            Assert.Throws<ArgumentNullException>(
+                () => JaccardIndex.CalculateJaccardIndex(null, new[] {0}));
+
+            Assert.Throws<ArgumentNullException>(
+                () => JaccardIndex.CalculateJaccardIndex<int>(null, null));
+        }
+
+        [Test]
+        public void CalculateJaccardIndex_SameSets()
+        {
+            var aSet = new[] {1};
+            var bSet = new[] {1};
+            var jaccardIndex = JaccardIndex.CalculateJaccardIndex(aSet, bSet);
+
+            Assert.AreEqual(100.0, jaccardIndex);
+        }
+
+        [Test]
+        public void CalculateJaccardIndex_CompletelyDifferentSets()
+        {
+            var aSet = new[] {0};
+            var bSet = new[] {1};
+            var jaccardIndex = JaccardIndex.CalculateJaccardIndex(aSet, bSet);
+
+            Assert.AreEqual(0, jaccardIndex);
+        }
+
+        [Test]
+        public void CalculateJaccardIndex_HalfwayDifferent()
+        {
+            var aSet = new[] {0, 1, 2, 5, 6};
+            var bSet = new[] {0, 2, 3, 4, 5, 7, 9};
+            var jaccardIndex = JaccardIndex.CalculateJaccardIndex(aSet, bSet);
+
+            Assert.AreEqual(33.33, jaccardIndex);
+        }
+        
+        
+        [Test]
+        public void CalculateJaccardIndex_DifferentOrder_ExpectSameJaccardIndex()
+        {
+            var aSet = new[] {0, 1, 2, 3, 4};
+            var bSet = aSet.Reverse().ToArray();
+            
+            var jaccardIndex = JaccardIndex.CalculateJaccardIndex(aSet, bSet);
+
+            Assert.AreEqual(100.00, jaccardIndex);
+        }
+    }
+}
---
+++ JaccardCalculator/JaccardIndex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JaccardCalculator
+{
+    public static class JaccardIndex
+    {
+        public static double CalculateJaccardIndex<T>(ICollection<T> a, ICollection<T> b)
+        {
+            CheckInput(a, b);
+
+            var intersectionCount = a.Intersect(b).Count();
+            var unionCount = a.Union(b).Count();
+
+            var jaccardIndex = (intersectionCount / (double) unionCount) * 100;
+            return Math.Round(jaccardIndex, 2);
+        }
+
+        private static void CheckInput<T>(ICollection<T> a, ICollection<T> b)
+        {
+            if (a == null)
+            {
+                throw new ArgumentNullException(nameof(a));
+            }
+
+            if (b == null)
+            {
+                throw new ArgumentNullException(nameof(b));
+            }
+        }
+    }
+}
